e5e42157938a64faa2e0b92a573ef176e1be94f4
Marta Małachowicz <malachowicz.marta@gmail.com>
2020-11-23 12:24:50 +0100

    express intro

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-const http = require('http');
+// const http = require('http');
 const PORT = 3000;
 //pętla do nasłuchiwania na określonym porcie
-http.createServer(function(request, response) {
-    const responseData = {
-        status: "ok!"
-    };
-    response.writeHead(200, {
-      'Content-Type': 'application/json'  
-    });
-    response.end(JSON.stringify(responseData));
-}).listen(PORT);
+// http.createServer(function(request, response) {
+//     const responseData = {
+//         status: "ok!"
+//     };
+//     response.writeHead(200, {
+//       'Content-Type': 'application/json'  
+//     });
+//     response.end(JSON.stringify(responseData));
+// }).listen(PORT);
 
-console.log(`Node server is running on port ${PORT}`);
+// console.log(`Node server is running on port ${PORT}`);
+
+
+//express korzystał z tego modułu http
+const express = require('express');
+const app = express();
+
+//GET 
+app.get('/', (req, res) => {
+    res.send("hello")
+});
+
+
+
+
+app.listen(PORT);
